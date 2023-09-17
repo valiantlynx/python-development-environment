4e7fc32170baaa20fe2f219944c4fae41e548a3b
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-09-17 22:47:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# a dev environment for python
+
+```bash
+docker-compose down --build -d
+docker-compose down
+```
---
+++ src/main.py
@@ -0,0 +1,16 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+ 
+import debugpy
+debugpy.listen(("0.0.0.0", 5678))
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World1 ass wiper"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
